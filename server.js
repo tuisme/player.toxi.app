55caf410c7d401b80d420724af2ab1683ff4e569
minhtu <minhtu.bk14@gmail.com>
2020-10-29 13:07:04 +0700

    init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const PORT = Number(process.env.PORT || 8888);
+const PORT = Number(process.env.PORT || 3000);
 const express = require('express');
 const bodyParser = require('body-parser');
 
